cf56e47d455460e12cfbbc0182bcdb88686ee36c
KeyofBlueS <45725677+KeyofBlueS@users.noreply.github.com>
2020-04-03 19:29:22 +0200

    fix change language in options menu
    fix change language in options menu
    other minor improvements

--- ui_boost.sh
+++ ui_boost.sh
@@ -2,7 +2,7 @@
 
 # UI-Boost airgeddon plugin
 
-# Version:    0.0.4
+# Version:    0.1.0
 # Author:     KeyofBlueS
 # Repository: https://github.com/KeyofBlueS/airgeddon-plugins
 # License:    GNU General Public License v3.0, https://opensource.org/licenses/GPL-3.0
@@ -10,7 +10,7 @@
 # airgeddon Repository: https://github.com/v1s1t0r1sh3r3/airgeddon
 
 #Global shellcheck disabled warnings
-#shellcheck disable=SC2034
+#shellcheck disable=SC2034,SC2154
 
 plugin_name="UI-Boost"
 plugin_description="Speed up user interface by making a specifc language strings file"
@@ -25,58 +25,103 @@ plugin_distros_supported=("*")
 #Custom function to make specifc language strings file
 function make_specifc_language_strings() {
 
-	# languages supported by airgeddon
-	languages="$(cat "${scriptfolder}${language_strings_file}" | grep "]=" | awk -F'"' '{print $2}' | awk -F'"' '{print $1}' | sort | uniq | awk '{print}' ORS=' ')"
-	#languages="ENGLISH SPANISH FRENCH CATALAN PORTUGUESE RUSSIAN GREEK ITALIAN POLISH GERMAN TURKISH"
-
 	# detect languages to be excluded
-	for language_to_exclude in $languages; do
-		if [ "$language_to_exclude" = "$language" ]; then
+	if [ -n "${languages_to_exclude}" ]; then
+		unset languages_to_exclude
+	fi
+	for language_to_exclude in ${languages}; do
+		if [ "${language_to_exclude}" = "${language}" ]; then
 			true
 		else
-			if [ -z "$languages_to_exclude" ]; then
-				languages_to_exclude=$language_to_exclude
+			if [ -z "${languages_to_exclude}" ]; then
+				languages_to_exclude="${language_to_exclude}"
 			else
-				languages_to_exclude="$languages_to_exclude|$language_to_exclude"
+				languages_to_exclude="${languages_to_exclude}|${language_to_exclude}"
 			fi
 		fi
 	done
 	
 	# make specifc language strings file
-	touch "${scriptfolder}"language_strings_${language}.sh
-	cat "${scriptfolder}${language_strings_file}" | grep -Ev "$languages_to_exclude" > ${scriptfolder}language_strings_${language}.sh
+	touch "${scriptfolder}${language_strings_file_complete%.*}"_"${language}".sh && chmod +x "${scriptfolder}${language_strings_file_complete%.*}"_"${language}".sh
+	cat "${scriptfolder}${language_strings_file_complete}" | grep -Ev "${languages_to_exclude}" > "${scriptfolder}${language_strings_file_complete%.*}"_"${language}".sh
 	
 	# setting new language_strings_file variable
-	language_strings_file="language_strings_${language}.sh"
+	language_strings_file="${language_strings_file_complete%.*}"_"${language}".sh
 	source "${scriptfolder}${language_strings_file}"
+	# update current language
+	language_current="${language}"
 }
 
-#Posthook to check if specifc language strings file exist and it's coherence
-#function ui_boost_posthook_check_language_strings() {			# THIS WILL NOT WORK
-function ui_boost_posthook_remap_colors() {
+#Custom function to check if specifc language strings file exist and it's coherence
+function check_specifc_language_strings() {
 
-	# detect scriptfolder if needed
-	if [ -z "${scriptfolder}" ]; then
-		scriptfolder=${0}
-
-		if ! [[ ${0} =~ ^/.*$ ]]; then
-			if ! [[ ${0} =~ ^.*/.*$ ]]; then
-				scriptfolder="./"
-			fi
-		fi
-		scriptfolder="${scriptfolder%/*}/"
-	fi
-
-	# check if specifc language strings file exist and it's coherence
-	if [ -e "${scriptfolder}language_strings_${language}.sh" ]; then
+	if [ -e "${scriptfolder}${language_strings_file_complete%.*}"_"${language}".sh ]; then
 		unset language_strings_version
 		unset set_language_strings_version
-		source "${scriptfolder}language_strings_${language}.sh"
+		source "${scriptfolder}${language_strings_file_complete%.*}"_"${language}".sh
 		set_language_strings_version
 		if [ "${language_strings_version}" != "${language_strings_expected_version}" ]; then
 			make_specifc_language_strings
 		fi
 	else
 		make_specifc_language_strings
+	fi
+}
+
+#Posthook to check if specifc language strings file exist and it's coherence
+function ui_boost_posthook_remap_colors() {
+
+	# store current language
+	language_current="${language}"
+	# store language_strings_file
+	language_strings_file_complete="${language_strings_file}"	
+	# languages supported by airgeddon
+	languages="$(cat "${scriptfolder}${language_strings_file_complete}" | grep "]=" | awk -F'"' '{print $2}' | awk -F'"' '{print $1}' | sort | uniq | awk '{print}' ORS=' ')"
+	#languages="ENGLISH SPANISH FRENCH CATALAN PORTUGUESE RUSSIAN GREEK ITALIAN POLISH GERMAN TURKISH"
+	check_specifc_language_strings
+}
+
+#Prehook to let user change language in options menu
+function ui_boost_prehook_language_menu() {
+
+	declare -gA arr
+	arr["ENGLISH",83]="Language changed to English"
+	arr["SPANISH",83]="Idioma cambiado a Español"
+	arr["FRENCH",83]="Le script sera maintenant en Français"
+	arr["CATALAN",83]="Idioma canviat a Català"
+	arr["PORTUGUESE",83]="Idioma alterado para Português"
+	arr["RUSSIAN",83]="Язык изменён на русский"
+	arr["GREEK",83]="Η γλώσσα άλλαξε στα Ελληνικά"
+	arr["ITALIAN",83]="Lingua cambiata in Italiano"
+	arr["POLISH",83]="Zmieniono język na Polski"
+	arr["GERMAN",83]="Sprache wurde auf Deutsch geändert"
+	arr["TURKISH",83]="Dil Türkçe olarak değiştirildi"
+	
+	arr["ENGLISH",115]="Press [Enter] key to continue..."
+	arr["SPANISH",115]="Pulsa la tecla [Enter] para continuar..."
+	arr["FRENCH",115]="Pressez [Enter] pour continuer..."
+	arr["CATALAN",115]="Prem la tecla [Enter] per continuar..."
+	arr["PORTUGUESE",115]="Pressione a tecla [Enter] para continuar..."
+	arr["RUSSIAN",115]="Нажмите клавишу [Enter] для продолжения..."
+	arr["GREEK",115]="Πατήστε το κουμπί [Enter] για να συνεχίσετε..."
+	arr["ITALIAN",115]="Premere il tasto [Enter] per continuare..."
+	arr["POLISH",115]="Naciśnij klawisz [Enter], aby kontynuować..."
+	arr["GERMAN",115]="Drücken Sie die [Enter]-Taste, um fortzufahren..."
+	arr["TURKISH",115]="Devam etmek için [Enter] tuşlayınız..."
+
+	arr["ENGLISH",251]="You have chosen the same language that was selected. No changes will be done"
+	arr["SPANISH",251]="Has elegido el mismo idioma que estaba seleccionado. No se realizarán cambios"
+	arr["FRENCH",251]="Vous venez de choisir la langue qui est en usage. Pas de changements"
+	arr["CATALAN",251]="Has triat el mateix idioma que estava seleccionat. No es realitzaran canvis"
+	arr["PORTUGUESE",251]="Você escolheu o mesmo idioma que estava selecionado. Nenhuma alteração será feita"
+	arr["RUSSIAN",251]="Вы выбрали такой же язык, какой и был. Никаких изменений не будет сделано"
+	arr["GREEK",251]="Επιλέξατε την ίδια γλώσσα που ήταν ήδη επιλεγμένη. Δεν θα γίνει καμία αλλαγή"
+	arr["ITALIAN",251]="Hai scelto la stessa lingua che è giá selezionata. Non sará effettutata nessuna modifica"
+	arr["POLISH",251]="Wybrałeś ten sam język, który jest używany. Żadne zmiany nie zostaną wprowadzone"
+	arr["GERMAN",251]="Sie haben die selbe Sprache ausgewählt. Es werden keine Änderungen vorgenommen"
+	arr["TURKISH",251]="Seçilmiş olan dili seçtiniz. Hiçbir değişiklik yapılmayacak"
+	
+	if [ "${language_current}" != "${language}" ]; then
+		check_specifc_language_strings
 	fi
 }
