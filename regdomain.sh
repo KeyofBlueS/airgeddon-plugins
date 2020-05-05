686fe395c6580b6aec5cb51b098d6ec6242e8b5e
KeyofBlueS <45725677+KeyofBlueS@users.noreply.github.com>
2020-05-05 03:24:20 +0200

    - Add languages support
    - Add languages support
    - Bump plugin_minimum_ag_affected_version to 10.20
    - Minor improvements

--- regdomain.sh
+++ regdomain.sh
@@ -2,7 +2,7 @@
 
 # Regdomain airgeddon plugin
 
-# Version:    0.0.1
+# Version:    0.0.2
 # Author:     KeyofBlueS
 # Repository: https://github.com/KeyofBlueS/airgeddon-plugins
 # License:    GNU General Public License v3.0, https://opensource.org/licenses/GPL-3.0
@@ -12,13 +12,13 @@
 #Global shellcheck disabled warnings
 #shellcheck disable=SC2034,SC2154
 
-plugin_name="Regdomain"
+regdomain="Regdomain"
 plugin_description="Set regulatory domain to affect the availability of wireless channels and txpower"
 plugin_author="KeyofBlueS"
 
 plugin_enabled=1
 
-plugin_minimum_ag_affected_version="10.0"
+plugin_minimum_ag_affected_version="10.20"
 plugin_maximum_ag_affected_version=""
 plugin_distros_supported=("*")
 
@@ -29,6 +29,10 @@ regulatory_domain=BZ
 
 #Custom function to set regulatory domain
 function set_regulatory_domain() {
+
+	debug_print
+
+	language_strings "${language}" "regdomain_text_0" "blue"
 
 	#Get current regulatory domain
 	current_regulatory_domain="$(iw reg get | grep "country" | awk -F' ' '{print $2}' | awk -F':' '{print $1}')"
@@ -56,23 +60,25 @@ function set_regulatory_domain() {
 		#Check regulatory domain again
 		current_regulatory_domain="$(iw reg get | grep "country" | awk -F' ' '{print $2}' | awk -F':' '{print $1}')"
 		if [ "${current_regulatory_domain}" != "${regulatory_domain}" ]; then
-			echo -e "\e[1;31mError setting Regulatory Domain: ${current_regulatory_domain}\e[0m"
-		else
-			echo -e "\e[1;32mRegulatory Domain: ${current_regulatory_domain}\e[0m"
+			language_strings "${language}" "regdomain_text_1" "red"
 		fi
-	else
-		echo -e "\e[1;32mRegulatory Domain: ${current_regulatory_domain}\e[0m"
 	fi
+	language_strings "${language}" "regdomain_text_2" "yellow"
+	echo_yellow "${current_regulatory_domain}"
 }
 
 #Prehook to set regulatory domain when setting interface in monitor mode
 function regdomain_prehook_monitor_option() {
+
+	debug_print
 
 	set_regulatory_domain
 }
 
 #Prehook to set regulatory domain when setting interface in managed mode
 function regdomain_prehook_managed_option() {
+
+	debug_print
 
 	set_regulatory_domain
 }
@@ -105,3 +111,47 @@ function regdomain_override_set_mode_without_airmon() {
 	fi
 	return 0
 }
+
+#Custom function. Create text messages to be used in regdomain plugin
+function initialize_regdomain_language_strings() {
+
+	debug_print
+
+	arr["ENGLISH","regdomain_text_0"]="Setting regulatory domain..."
+	arr["SPANISH","regdomain_text_0"]="\${pending_of_translation} Establecer dominio regulador..."
+	arr["FRENCH","regdomain_text_0"]="\${pending_of_translation} Définition du domaine réglementaire..."
+	arr["CATALAN","regdomain_text_0"]="\${pending_of_translation} Configuració del domini regulador..."
+	arr["PORTUGUESE","regdomain_text_0"]="\${pending_of_translation} Definindo domínio regulador..."
+	arr["RUSSIAN","regdomain_text_0"]="\${pending_of_translation} Настройка нормативного домена..."
+	arr["GREEK","regdomain_text_0"]="\${pending_of_translation} Ορισμός ρυθμιστικού τομέα..."
+	arr["ITALIAN","regdomain_text_0"]="Imposto il regulatory domain..."
+	arr["POLISH","regdomain_text_0"]="\${pending_of_translation} Ustawianie domeny regulacyjnej..."
+	arr["GERMAN","regdomain_text_0"]="\${pending_of_translation} Festlegen der regulatorischen Domäne..."
+	arr["TURKISH","regdomain_text_0"]="\${pending_of_translation} Yasal alan adı ayarlanıyor..."
+
+	arr["ENGLISH","regdomain_text_1"]="Error while setting regulatory domain!"
+	arr["SPANISH","regdomain_text_1"]="\${pending_of_translation} ¡Error al configurar el dominio regulador!"
+	arr["FRENCH","regdomain_text_1"]="\${pending_of_translation} Erreur lors de la définition du domaine réglementaire!"
+	arr["CATALAN","regdomain_text_1"]="\${pending_of_translation} S'ha produït un error en configurar el domini regulador!"
+	arr["PORTUGUESE","regdomain_text_1"]="\${pending_of_translation} Erro ao definir o domínio regulatório!"
+	arr["RUSSIAN","regdomain_text_1"]="\${pending_of_translation} Ошибка при настройке регуляторного домена!"
+	arr["GREEK","regdomain_text_1"]="\${pending_of_translation} Σφάλμα κατά τη ρύθμιση του ρυθμιστικού τομέα!"
+	arr["ITALIAN","regdomain_text_1"]="Errore durante l'impostazione del regulatory domain!"
+	arr["POLISH","regdomain_text_1"]="\${pending_of_translation} Błąd podczas ustawiania domeny regulacyjnej!"
+	arr["GERMAN","regdomain_text_1"]="\${pending_of_translation} Fehler beim Einstellen der Regulierungsdomäne!"
+	arr["TURKISH","regdomain_text_1"]="\${pending_of_translation} Yasal alan adı ayarlanırken hata oluştu!"
+
+	arr["ENGLISH","regdomain_text_2"]="Current regulatory domain is:"
+	arr["SPANISH","regdomain_text_2"]="\${pending_of_translation} El dominio regulador actual es:"
+	arr["FRENCH","regdomain_text_2"]="\${pending_of_translation} Le domaine réglementaire actuel est:"
+	arr["CATALAN","regdomain_text_2"]="\${pending_of_translation} El domini regulador actual és:"
+	arr["PORTUGUESE","regdomain_text_2"]="\${pending_of_translation} O domínio regulatório atual é:"
+	arr["RUSSIAN","regdomain_text_2"]="\${pending_of_translation} Текущий регуляторный домен:"
+	arr["GREEK","regdomain_text_2"]="\${pending_of_translation} Ο τρέχων κανονιστικός τομέας είναι:"
+	arr["ITALIAN","regdomain_text_2"]="L'attuale regulatory domain è:"
+	arr["POLISH","regdomain_text_2"]="\${pending_of_translation} Obecna domena regulacyjna to:"
+	arr["GERMAN","regdomain_text_2"]="\${pending_of_translation} Aktuelle regulatorische Domäne ist:"
+	arr["TURKISH","regdomain_text_2"]="\${pending_of_translation} Mevcut yasal alan adı:"
+}
+
+initialize_regdomain_language_strings
